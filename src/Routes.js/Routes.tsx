16b8096e6fb54c6481d5346cf7b4b873015387af
amaben2020 <amaben202phantom@gmail.com>
2021-08-03 21:21:09 +0100

    Added bootstrap spinner to fallback suspense

--- src/Routes.js/Routes.tsx
+++ src/Routes.js/Routes.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { Spinner } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 const LandingPage = React.lazy(() => import("./../components/LandingPage"));
 const Routes = () => {
   return (
-    <React.Suspense fallback={<p>Loading....</p>}>
+    <React.Suspense
+      fallback={
+        <p>
+          <Spinner animation="border" />
+        </p>
+      }
+    >
       <Router>
         <Switch>
           <Route exact path="/">
-            {" "}
-            <LandingPage />{" "}
+            <LandingPage />
           </Route>
         </Switch>
       </Router>
